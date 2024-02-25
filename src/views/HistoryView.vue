ea11beb6837eaa2253d6968caa81b44ac33c234e
Pattarapol Chaijam <64160249@go.buu.ac.thcls>
2024-02-25 22:43:21 +0700

    update dailog

--- src/views/HistoryView.vue
+++ src/views/HistoryView.vue
@@ -38,6 +38,13 @@ const formatShowDate = (dateTime: Date | undefined): string => {
   return formattedDate
 }
 
+const selectedReceipt = ref<Receipt>()
+
+const handleDetailButtonClick = (receipt: Receipt) => {
+  selectedReceipt.value = receipt
+  movieHistoryDetailDialog.value = true
+}
+
 const formatTime = (seconds: number) => {
   const minutes = Math.floor(seconds / 60)
   const remainingSeconds = seconds % 60
@@ -102,7 +109,7 @@ onMounted(async () => {
               :width="150"
               :height="40"
               class="mt-6 ml-7"
-              @click="movieHistoryDetailDialog = true"
+              @click="handleDetailButtonClick(receipt)"
               style="
                 background: linear-gradient(to right, #b91c1c, #ff6640);
                 color: white;
@@ -130,134 +137,156 @@ onMounted(async () => {
         </v-row>
       </v-col>
       <v-divider style="border: 1px solid black" class="mt-3 mb-3 border-opacity-20"></v-divider>
-      <v-dialog v-model="movieHistoryDetailDialog" width="auto">
-        <v-card :width="1200" :height="700">
-          <v-row>
-            <v-col cols="2" class="justify-center">
-              <v-card
-                class="ml-10 mt-5"
-                style="border-radius: 1rem; background: #b91c1c"
-                :width="190"
-                :height="265"
-              >
-                <v-img
-                  class="mt-2"
-                  style="border-radius: 1rem"
-                  :width="200"
-                  :height="250"
-                  :src="`http://localhost:3000/movies/${receipt.tickets[0].showtime.movie.movieId}/image`"
-                ></v-img>
-              </v-card>
-            </v-col>
-            <v-col cols="10">
-              <v-row>
-                <v-col cols="8" class="d-flex flex-column mt-12 ml-16">
-                  <h1 class="mb-2 mt-5 ml-7" style="color: black">
-                    {{ receipt.tickets[0].showtime.movie.movieName }}
-                  </h1>
-                  <p style="color: black; font-size: medium" class="ml-7">
-                    <v-icon>{{ mdiVolumeHigh }}</v-icon
-                    >ENG/TH
-                  </p>
-                </v-col>
-                <v-col class="d-flex flex-column mt-12 ml-16">
-                  <p style="color: black; font-weight: 600; font-size: larger" class="mt-5 ml-7">
-                    ราคารวม
-                  </p>
-                  <p class="ml-7" style="color: #b91c1c; font-weight: 600; font-size: 30px">
-                    {{ receipt.tickets[0].ticketPrice }}฿
-                  </p>
-                </v-col>
-              </v-row>
-              <v-divider
-                style="border: 1px solid black"
-                class="mt-3 mb-3 ml-16 mr-5 border-opacity-40"
-              ></v-divider>
-              <v-row>
-                <v-col cols="3" class="d-flex flex-column ml-16">
-                  <p style="color: #b91c1c; font-weight: 600; font-size: larger" class="ml-7 mb-2">
-                    <v-icon>{{ mdiMapMarker }}</v-icon
-                    >สกาลาบางแสน
-                  </p>
-                </v-col>
-              </v-row>
-              <v-row>
-                <v-col cols="4" class="d-flex flex-column ml-16">
-                  <p style="color: black; font-weight: 600; font-size: medium" class="ml-10">
-                    โรงภาพยนต์
-                  </p>
-                  <p class="ml-16 pl-4" style="color: #b91c1c; font-weight: 600; font-size: 25px">
-                    1
-                  </p>
-                </v-col>
-                <v-col cols="5" class="d-flex flex-column ml-10">
-                  <p style="color: black; font-weight: 600; font-size: medium" class="ml-7">
-                    ที่นั่ง
-                  </p>
-                  <p class="ml-2" style="color: #b91c1c; font-weight: 600; font-size: 25px">
-                    {{ receipt.tickets[0].seat }}
-                  </p>
-                </v-col>
-              </v-row>
-              <v-row class="mt-7">
-                <v-col cols="4" class="d-flex flex-column ml-16">
-                  <p style="color: black; font-weight: 600; font-size: medium" class="ml-10 pl-8">
-                    วันที่
-                  </p>
-                  <p class="ml-5" style="color: #b91c1c; font-weight: 600; font-size: 16px">
-                    {{ formatShowDate(receipt.tickets[0].showtime.showDate) }}
-                  </p>
-                </v-col>
-                <v-col cols="4" class="d-flex flex-column ml-10">
-                  <p style="color: black; font-weight: 600; font-size: medium" class="ml-7">เวลา</p>
-                  <p class="ml-5" style="color: #b91c1c; font-weight: 600; font-size: 16px">
-                    {{ getFormattedTime(new Date(receipt.tickets[0].showtime.showStart)) }}
-                  </p>
-                </v-col>
-              </v-row>
-              <v-row class="mt-7">
-                <v-col cols="8" class="d-flex flex-column ml-16">
-                  <p style="color: black; font-weight: 600; font-size: medium" class="ml-6">
-                    อาหาร/เครื่องดื่ม
-                  </p>
-                  <p class="ml-0" style="color: #b91c1c; font-weight: 600; font-size: 16px">
-                    {{ receipt.recfoods[0].food.foodName }} x
-                    <span>1</span>
-                  </p>
-                </v-col>
-              </v-row>
-              <v-row class="mt-7">
-                <v-col cols="8" class="d-flex flex-column ml-16">
-                  <p style="color: black; font-weight: 600; font-size: medium" class="ml-6">
-                    รหัสการซื้อ
-                  </p>
-                  <p class="ml-5" style="color: #b91c1c; font-weight: 600; font-size: 25px">
-                    {{ receipt.tickets[0].ticketNumber }}
-                  </p>
-                </v-col>
-                <v-col class="d-flex flex-column">
-                  <p
-                    class="mt-15"
-                    style="color: #b91c1c; font-weight: 600; font-size: 25px; text-align: center"
-                  >
-                    ชำระเงินแล้ว
-                  </p>
-                </v-col>
-              </v-row>
-            </v-col>
-          </v-row>
-
-          <v-card-actions>
-            <v-btn
-              block
-              @click="movieHistoryDetailDialog = false"
-              style="font-weight: bold"
-              color="#b91c1c"
-              >ปิด</v-btn
-            >
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
     </v-row>
+    <v-dialog v-model="movieHistoryDetailDialog" width="auto">
+      <v-card :width="1200" :height="700">
+        <v-row>
+          <v-col cols="2" class="justify-center">
+            <v-card
+              class="ml-10 mt-5"
+              style="border-radius: 1rem; background: #b91c1c"
+              :width="190"
+              :height="265"
+            >
+              <v-img
+                class="mt-2"
+                style="border-radius: 1rem"
+                :width="200"
+                :height="250"
+                :src="`http://localhost:3000/movies/${selectedReceipt?.tickets[0].showtime.movie.movieId}/image`"
+              ></v-img>
+            </v-card>
+          </v-col>
+          <v-col cols="10">
+            <v-row>
+              <v-col cols="8" class="d-flex flex-column mt-12 ml-16">
+                <h1 class="mb-2 mt-5 ml-7" style="color: black">
+                  {{ selectedReceipt?.tickets[0].showtime.movie.movieName }}
+                </h1>
+                <p style="color: black; font-size: medium" class="ml-7">
+                  <v-icon>{{ mdiVolumeHigh }}</v-icon
+                  >ENG/TH
+                </p>
+              </v-col>
+              <v-col class="d-flex flex-column mt-12 ml-16">
+                <p style="color: black; font-weight: 600; font-size: larger" class="mt-5 ml-7">
+                  ราคารวม
+                </p>
+                <p class="ml-7" style="color: #b91c1c; font-weight: 600; font-size: 30px">
+                  {{ selectedReceipt?.tickets[0].ticketPrice }}฿
+                </p>
+              </v-col>
+            </v-row>
+            <v-divider
+              style="border: 1px solid black"
+              class="mt-3 mb-3 ml-16 mr-5 border-opacity-40"
+            ></v-divider>
+            <v-row>
+              <v-col cols="3" class="d-flex flex-column ml-16">
+                <p style="color: #b91c1c; font-weight: 600; font-size: larger" class="ml-7 mb-2">
+                  <v-icon>{{ mdiMapMarker }}</v-icon
+                  >สกาลาบางแสน
+                </p>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="4" class="d-flex flex-column ml-16">
+                <p style="color: black; font-weight: 600; font-size: medium" class="ml-10">
+                  โรงภาพยนต์
+                </p>
+                <p class="ml-16 pl-4" style="color: #b91c1c; font-weight: 600; font-size: 25px">
+                  1
+                </p>
+              </v-col>
+              <v-col cols="5" class="d-flex flex-column ml-10">
+                <p style="color: black; font-weight: 600; font-size: medium" class="ml-7">
+                  ที่นั่ง
+                </p>
+                <p class="ml-2" style="color: #b91c1c; font-weight: 600; font-size: 25px">
+                  {{ selectedReceipt?.tickets[0].showtime.showtimeseats }}
+                </p>
+              </v-col>
+            </v-row>
+            <v-row class="mt-7">
+              <v-col cols="4" class="d-flex flex-column ml-16">
+                <p style="color: black; font-weight: 600; font-size: medium" class="ml-10 pl-8">
+                  วันที่
+                </p>
+                <p class="ml-5" style="color: #b91c1c; font-weight: 600; font-size: 16px">
+                  {{ formatShowDate(selectedReceipt?.tickets[0].showtime.showDate) }}
+                </p>
+              </v-col>
+              <v-col cols="4" class="d-flex flex-column ml-10">
+                <p style="color: black; font-weight: 600; font-size: medium" class="ml-7">เวลา</p>
+                <p class="ml-5" style="color: #b91c1c; font-weight: 600; font-size: 16px">
+                  {{
+                    getFormattedTime(
+                      new Date(String(selectedReceipt?.tickets[0].showtime.showStart))
+                    )
+                  }}
+                </p>
+              </v-col>
+            </v-row>
+            <v-row class="mt-7">
+              <v-col cols="8" class="d-flex flex-column ml-16">
+                <p style="color: black; font-weight: 600; font-size: medium" class="ml-6">
+                  อาหาร/เครื่องดื่ม
+                </p>
+                <p
+                  style="color: #b91c1c; font-weight: 600; font-size: 16px"
+                  v-if="!selectedReceipt?.recfoods.length"
+                  class="ml-16 pl-4"
+                >
+                  {{
+                    selectedReceipt?.recfoods.length
+                      ? selectedReceipt?.recfoods
+                          .map((food) => `${food.food.foodName} x ${food.recFoodQty}`)
+                          .join(', ')
+                      : '-'
+                  }}
+                </p>
+                <p style="color: #b91c1c; font-weight: 600; font-size: 16px" v-else class="ml-0">
+                  {{
+                    selectedReceipt?.recfoods.length
+                      ? selectedReceipt?.recfoods
+                          .map((food) => `${food.food.foodName} x${food.recFoodQty}`)
+                          .join(', ')
+                      : '-'
+                  }}
+                </p>
+              </v-col>
+            </v-row>
+            <v-row class="mt-7">
+              <v-col cols="8" class="d-flex flex-column ml-16">
+                <p style="color: black; font-weight: 600; font-size: medium" class="ml-6">
+                  รหัสการซื้อ
+                </p>
+                <p class="ml-5" style="color: #b91c1c; font-weight: 600; font-size: 25px">
+                  {{ selectedReceipt?.tickets[0].ticketNumber }}
+                </p>
+              </v-col>
+              <v-col class="d-flex flex-column">
+                <p
+                  class="mt-15"
+                  style="color: #b91c1c; font-weight: 600; font-size: 25px; text-align: center"
+                >
+                  ชำระเงินแล้ว
+                </p>
+              </v-col>
+            </v-row>
+          </v-col>
+        </v-row>
+
+        <v-card-actions>
+          <v-btn
+            block
+            @click="movieHistoryDetailDialog = false"
+            style="font-weight: bold"
+            color="#b91c1c"
+            >ปิด</v-btn
+          >
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-container>
 </template>
